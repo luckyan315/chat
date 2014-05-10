61d2bda291423ab83f0274ce6a4a77423ca4ffb4
luckyan315 <luckyan315@qq.com>
2014-05-10 11:30:06 +0800

    fix indent

--- test/chat.test.js
+++ test/chat.test.js
@@ -4,29 +4,29 @@
  * 
  */
 
- "use strict";
+"use strict";
 
- var should = require('should');
- var request = require('supertest');
- var http = require('http');
- var ioc = require('socket.io-client');
- var async = require('async');
+var should = require('should');
+var request = require('supertest');
+var http = require('http');
+var ioc = require('socket.io-client');
+var async = require('async');
 
- var debug = require('debug')('chat:test');
- var _ = require('lodash');
+var debug = require('debug')('chat:test');
+var _ = require('lodash');
 
- var chat = require('../chat.js');
- var app = chat.app;
- var sio = chat.io;
- var httpServer = chat.httpServer;
+var chat = require('../chat.js');
+var app = chat.app;
+var sio = chat.io;
+var httpServer = chat.httpServer;
 
- var config = require('../config');
- var host = config.test.host;
- var port = config.test.port;
+var config = require('../config');
+var host = config.test.host;
+var port = config.test.port;
 
- describe('Chat Server', function(){
+describe('Chat Server', function(){
   var server = null;
-  var address = 'ws://' + host + ':' + port;
+      var address = 'ws://' + host + ':' + port;
   var room_name = 'baywalk';
 
   debug('[address]: ' + address);
@@ -59,28 +59,28 @@
   
   it('should got 200 status code when visit get /', function(done){
     request(app)
-    .get('/')
-    .expect(200, done);
+      .get('/')
+      .expect(200, done);
   });
 
   it('should work as a static server', function(done){
     request(app)
-    .get('/index.html')
-    .expect(200, done);
+      .get('/index.html')
+      .expect(200, done);
   });
 
   it('should "Not Found(404)"if there is not static file', function(done){
     request(app)
-    .get('/xxx.html')
-    .expect(404)
-    .end(function(err, res){
-      done();
-    });
+      .get('/xxx.html')
+      .expect(404)
+      .end(function(err, res){
+        done();
+      });
   });
   
   it('should connect the server success', function(done){
     // var mgr = ioc.Manager(address + '/user');
-    var user_socket = ioc(address + '/user', { transports: ['websocket'] });
+        var user_socket = ioc(address + '/user', { transports: ['websocket'] });
     user_socket.on('connect', function(){
       user_socket.io.engine.close();
       done();
@@ -115,7 +115,7 @@
     var nTimes = 3;
     var mgr = ioc.Manager({ transports: ['websocket'], reconnection: true, timeout: 0, reconnectionAttempts: nTimes, reconnectionDelay: 50});
     var socket = mgr.socket('/');
-    var nReconn = 0;
+        var nReconn = 0;
 
     mgr.on('reconnect_attempt', function(){
       nReconn++;
@@ -146,7 +146,7 @@
   });
 
   it('should do add action via /user namespace', function(done){
-    var user_socket = ioc(address + '/user', { transports: ['websocket'], multiplex: false});
+        var user_socket = ioc(address + '/user', { transports: ['websocket'], multiplex: false});
     var bEmitted = false;
     
     user_socket.on('connect', function(){
@@ -154,15 +154,15 @@
       user_socket.emit('add', 'angl', function(err, data){
         bEmitted = true;
         if (err) throw new Error(err);
-         //emit once more
-         return user_socket.emit('add', 'angl', function(err, data){
+        //emit once more
+        return user_socket.emit('add', 'angl', function(err, data){
           if (err) {
-          user_socket.io.engine.close();            
-           return done();
-         }
-       });
-         
-       });
+            user_socket.io.engine.close();            
+            return done();
+          }
+        });
+        
+      });
     })
   });
 
@@ -194,7 +194,7 @@
 
       debug('', '[ioc] [user1] recv msg nRecv: ' + nRecv);
       if (nRecv === nExp){
-         user1_socket.io.engine.close();        
+        user1_socket.io.engine.close();        
         done();
       }
     });
@@ -214,79 +214,79 @@
     });
   });
 
-it('should sio broadcast all namespace ok', function(done){
-  var user_socket = ioc(address + '/user', { transports: ['websocket'], multiplex: false });
-  var pri_socket = ioc(address + '/private', { transports: ['websocket'], multiplex: false });
-  var nRecv = 0;
-  var nExp = 2; // expect result
-  var bEmitted = false;
+  it('should sio broadcast all namespace ok', function(done){
+    var user_socket = ioc(address + '/user', { transports: ['websocket'], multiplex: false });
+    var pri_socket = ioc(address + '/private', { transports: ['websocket'], multiplex: false });
+        var nRecv = 0;
+    var nExp = 2; // expect result
+    var bEmitted = false;
 
-  pri_socket.on('new message', function(data){
-    nRecv++;
+    pri_socket.on('new message', function(data){
+      nRecv++;
 
-    debug('', '[ioc] recv msg nRecv: ' + nRecv);
-    if(nRecv === nExp) done();
-    pri_socket.io.engine.close();
+      debug('', '[ioc] recv msg nRecv: ' + nRecv);
+      if(nRecv === nExp) done();
+      pri_socket.io.engine.close();
+    });
+
+    user_socket.on('new message', function(data){
+      nRecv++;
+
+      debug('', '[ioc] recv msg nRecv: ' + nRecv);
+      if(nRecv === nExp) done();
+
+      user_socket.io.engine.close();    
+    });
+
+    sio.of('/user').on('connection', function(socket){
+      if (bEmitted) return;    
+      user_socket.emit('broadcast namespace', 'hi all namespace');
+      bEmitted = true;
+    });
+
   });
 
-  user_socket.on('new message', function(data){
-    nRecv++;
+  it('should sio sockets broadcast a specified room ok', function(done){
+    var user1 = ioc(address, { transports: ['websocket'], multiplex: false });
+    var user2 = ioc(address, { transports: ['websocket'], multiplex: false });
+    var user3 = ioc(address, { transports: ['websocket'], multiplex: false });
+    var nRecv = 0;
 
-    debug('', '[ioc] recv msg nRecv: ' + nRecv);
-    if(nRecv === nExp) done();
+    user1.on('new message', function(data){
+      debug('[user1] recv msg: ' + data);
+      nRecv++;
+      if (nRecv === 2) done();      
+      user1.io.engine.close();
+    });
 
-    user_socket.io.engine.close();    
+    user2.on('new message', function(data){
+      debug('[user2] recv msg: ' + data);
+      nRecv++;
+      if (nRecv === 2) done(); 
+      user1.io.engine.close();
+    });
+
+    user3.on('new message', function(data){
+      debug('[user3] recv msg: ' + data);
+      user3.io.engine.close();
+      done(new Error('Should not recv msg by self'));
+    });
+
+
+    user1.emit('join room', room_name);
+    user2.emit('join room', room_name);
+    user3.emit('join room', room_name, function(){
+      user3.emit('broadcast room', 'hi all room', room_name);
+    });
+
   });
 
-  sio.of('/user').on('connection', function(socket){
-    if (bEmitted) return;    
-    user_socket.emit('broadcast namespace', 'hi all namespace');
-    bEmitted = true;
-  });
-
-});
-
-it('should sio sockets broadcast a specified room ok', function(done){
-  var user1 = ioc(address, { transports: ['websocket'], multiplex: false });
-  var user2 = ioc(address, { transports: ['websocket'], multiplex: false });
-  var user3 = ioc(address, { transports: ['websocket'], multiplex: false });
-  var nRecv = 0;
-
-  user1.on('new message', function(data){
-    debug('[user1] recv msg: ' + data);
-    nRecv++;
-    if (nRecv === 2) done();      
-    user1.io.engine.close();
-  });
-
-  user2.on('new message', function(data){
-    debug('[user2] recv msg: ' + data);
-    nRecv++;
-    if (nRecv === 2) done(); 
-    user1.io.engine.close();
-  });
-
-  user3.on('new message', function(data){
-    debug('[user3] recv msg: ' + data);
-    user3.io.engine.close();
-    done(new Error('Should not recv msg by self'));
-  });
-
-
-  user1.emit('join room', room_name);
-  user2.emit('join room', room_name);
-  user3.emit('join room', room_name, function(){
-    user3.emit('broadcast room', 'hi all room', room_name);
-  });
-
-});
-
-it('should sio sockets broadcast multi-room', function(done){
-  var user1 = ioc(address, { transports: ['websocket'], multiplex: false });
-  var user2 = ioc(address, { transports: ['websocket'], multiplex: false });
-  var user3 = ioc(address, { transports: ['websocket'], multiplex: false });
-  var user4 = ioc(address, { transports: ['websocket'], multiplex: false });
-  var nRecv = 0;
+  it('should sio sockets broadcast multi-room', function(done){
+    var user1 = ioc(address, { transports: ['websocket'], multiplex: false });
+    var user2 = ioc(address, { transports: ['websocket'], multiplex: false });
+    var user3 = ioc(address, { transports: ['websocket'], multiplex: false });
+    var user4 = ioc(address, { transports: ['websocket'], multiplex: false });
+    var nRecv = 0;
     var nExp = 3; //expect result
 
     user1.on('new message', function(data){
@@ -324,78 +324,79 @@ it('should sio sockets broadcast multi-room', function(done){
       user3.emit('broadcast room', 'hi all');
     });
 
-  })
+      })
 
-it('should add/leave room ok', function(done){
-  var user1 = ioc(address, { transports: ['websocket'], multiplex: false });
-  var user2 = ioc(address, { transports: ['websocket'], multiplex: false });
-  var user3 = ioc(address, { transports: ['websocket'], multiplex: false });
-  var nUsers = 0;
+  it('should add/leave room ok', function(done){
+    var user1 = ioc(address, { transports: ['websocket'], multiplex: false });
+    var user2 = ioc(address, { transports: ['websocket'], multiplex: false });
+    var user3 = ioc(address, { transports: ['websocket'], multiplex: false });
+    var nUsers = 0;
 
-  user1.emit('join room', room_name, function(){ nUsers++; if(nUsers === 3) user_leave();});
-  user2.emit('join room', room_name, function(){ nUsers++; if(nUsers === 3) user_leave();});
-  user3.emit('join room', room_name, function(){ nUsers++; if(nUsers === 3) user_leave();});
+    user1.emit('join room', room_name, function(){ nUsers++; if(nUsers === 3) user_leave();});
+    user2.emit('join room', room_name, function(){ nUsers++; if(nUsers === 3) user_leave();});
+    user3.emit('join room', room_name, function(){ nUsers++; if(nUsers === 3) user_leave();});
 
-  function user_leave(){
-    debug('one user leaving the room....' + room_name);
+        function user_leave(){
+          debug('one user leaving the room....' + room_name);
 
-    user1.emit('leave room', room_name, function(){
-      --nUsers;
-      var sids = sio.sockets.adapter.sids;
-      debug('[adapter.sids]: ', sids);
+          user1.emit('leave room', room_name, function(){
+            --nUsers;
+            var sids = sio.sockets.adapter.sids;
+            debug('[adapter.sids]: ', sids);
 
-      var nLeftRoom = _.reduce(sids, function(result, num, key){
-        if(sids[key][room_name] === true) result++;
-        
-        return result;
-      }, 0);
+            var nLeftRoom = _.reduce(sids, function(result, num, key){
+              if(sids[key][room_name] === true) result++;
+              
+              return result;
+            }, 0);
 
-      debug('[nLeftRoom]: ' + nLeftRoom + ' [nUsers]: ' + nUsers);
-      if (nLeftRoom === nUsers) {
-        user1.io.engine.close();
-        user2.io.engine.close();
-        user3.io.engine.close();
+            debug('[nLeftRoom]: ' + nLeftRoom + ' [nUsers]: ' + nUsers);
+                if (nLeftRoom === nUsers) {
+                  user1.io.engine.close();
+                      user2.io.engine.close();
+                  user3.io.engine.close();
 
-        done();        
-      }
-    });
-  }
-});
-
-it('should add multi users', function(done) {
-  var user1 = ioc(address + '/user', { transports: ['websocket'], multiplex: false });
-  var user2 = ioc(address + '/user', { transports: ['websocket'], multiplex: false });
-  var user3 = ioc(address + '/user', { transports: ['websocket'], multiplex: false });
-  var bEmitted = false;
-
-  sio.of('/user').on('connection', function(){
-    if (bEmitted) return;
-    async.series({
-      one: function(cb){
-        user1.emit('add', 'user1', function(err, data){
-          bEmitted = true;
-          cb(err, data);
-        });
-      },
-      two: function(cb){
-        user2.emit('add', 'user2', function(err, data){
-          cb(err, data);
-        });
-      },
-      three: function(cb){
-        user3.emit('add', 'user3', function(err, data){
-          cb(err, data);
-        });
-      }
-    }, function(err, result){
-      if(!err) {
-        user1.io.engine.close();
-        user2.io.engine.close();
-        user3.io.engine.close();
-
-        done();        
-      }
-    });      
+                  done();        
+                }
+          });
+        }
   });
-});
+
+  it('should add multi users', function(done) {
+    var user1 = ioc(address + '/user', { transports: ['websocket'], multiplex: false });
+    var user2 = ioc(address + '/user', { transports: ['websocket'], multiplex: false });
+    var user3 = ioc(address + '/user', { transports: ['websocket'], multiplex: false });
+    var bEmitted = false;
+
+    sio.of('/user').on('connection', function(){
+      if (bEmitted) return;
+      async.series({
+        one: function(cb){
+          user1.emit('add', 'user1', function(err, data){
+            bEmitted = true;
+            cb(err, data);
+          });
+        },
+        two: function(cb){
+          user2.emit('add', 'user2', function(err, data){
+            cb(err, data);
+          });
+        },
+        three: function(cb){
+          user3.emit('add', 'user3', function(err, data){
+            cb(err, data);
+          });
+        }
+      }, function(err, result){
+        if(!err) {
+          user1.io.engine.close();
+          user2.io.engine.close();
+          user3.io.engine.close();
+
+          done();        
+        }
+      });      
+    });
+  });
+  
 });
